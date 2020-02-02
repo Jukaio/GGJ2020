60e1f9184110a77d4f5ee855f29167575525ae13
David <David.Naussed.5395@student.uu.se>
2020-02-02 02:07:54 +0100

    GO

---
+++ GGJ2020/GGJ2020/Assets/Ease_Into_Scene.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Ease_Into_Scene : MonoBehaviour
+{
+    AudioSource audio_Source_;
+    public AudioClip clip_;
+
+    public Image image_;
+    public GameObject game_Object_;
+    public float t_ = 1.0f;
+    public float dt_;
+
+    float last_ = 0;
+    float now_ = 0; 
+
+    public Color black_;
+    bool start_ = false;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        audio_Source_ = (AudioSource)gameObject.AddComponent(typeof(AudioSource));
+
+        Time.timeScale = 0.0f;
+        last_ = now_;
+        now_ = Time.realtimeSinceStartup;
+        //game_Object_.SetActive(false);
+    }
+
+    private void Start()
+    { 
+        start_ = true;
+    }
+
+    void Update()
+    {
+        if (start_)
+        {
+            t_ -= dt_ / 2.0f;
+            if (t_ < 0.0f)
+            {
+                audio_Source_.clip = clip_;
+                audio_Source_.pitch = 1.05f;
+                audio_Source_.PlayDelayed(0.2f);
+                Time.timeScale = 1.0f;
+                this.enabled = false;
+            }
+            last_ = now_;
+            now_ = Time.realtimeSinceStartup;
+            dt_ = now_ - last_;
+
+            print(dt_);
+            black_.a = (float)EaseInCubic(t_);
+            image_.color = black_;
+
+        }
+    }
+
+    public double EaseInCubic(double t)
+    {
+        return t * t * t;
+    }
+}
---
+++ GGJ2020/GGJ2020/Assets/Vicotry_Screen.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+public class Vicotry_Screen : MonoBehaviour
+{
+    public Image image_;
+
+    public bool pressed_ = false;
+    public float rotation_ = 0;
+    // Start is called before the first frame update
+    public float factor_ = 2.0f;
+    float t_ = 0;
+
+    Color color_;
+    void Awake()
+    {
+        color_ = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    float ease_;
+    // Update is called once per frame
+    void Update()
+    { 
+        if (Input.GetKeyDown(KeyCode.Space) && pressed_ == false)
+        {
+            pressed_ = true;
+        }
+
+        if(pressed_ == true)
+        {
+            Quaternion test = Quaternion.Euler(0.0f, 0.0f, rotation_);
+            gameObject.transform.rotation = test;
+
+            ease_ = (float)EaseInCubic(t_);
+
+            rotation_ += factor_ * ease_ * 3.0f;
+            color_.a = ease_;
+            image_.color = color_;
+
+            t_ += Time.deltaTime / 1.5f;
+            if (t_ > 1.0f)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    public double EaseInCubic(double t)
+    {
+        return t * t * t;
+    }
+
+}
